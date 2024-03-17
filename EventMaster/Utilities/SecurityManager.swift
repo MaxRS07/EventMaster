8f9131a835bd8ada0c637d8564f242c2aebcc840
Max <Max@siebengartner.com>
2024-03-17 18:43:36 +0800

    More UI

---
+++ EventMaster/Utilities/SecurityManager.swift
@@ -0,0 +1,8 @@
+//
+//  SecurityManager.swift
+//  EventMaster
+//
+//  Created by Max Siebengartner on 17/3/2024.
+//
+
+import Foundation
---
+++ EventMaster/Views/EventCard.swift
@@ -0,0 +1,8 @@
+//
+//  EventCard.swift
+//  EventMaster
+//
+//  Created by Max Siebengartner on 17/3/2024.
+//
+
+import Foundation
